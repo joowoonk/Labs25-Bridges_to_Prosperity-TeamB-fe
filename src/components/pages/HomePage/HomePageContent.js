d4af7137e86bc30fa8fcc68edad7e5ce261c4ec5
Joo Woon Kang <49606018+joowoonk@users.noreply.github.com>
2021-04-07 17:17:35 -0700

    Update HomePageContent.js

--- src/components/pages/HomePage/HomePageContent.js
+++ src/components/pages/HomePage/HomePageContent.js
@@ -9,7 +9,7 @@ const HomePageReact = () => {
   const { setBridgeData } = useContext(BridgesContext);
 
   useEffect(() => {
-    getDSData('https://bridges-b-api.herokuapp.com/bridges').then(data => {
+    getDSData('https://bridges-to-prosperity.herokuapp.com').then(data => {
       setBridgeData(data);
     });
   }, [setBridgeData]);
